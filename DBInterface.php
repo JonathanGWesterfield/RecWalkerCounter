76679303d93689452396b767fa0898f7bcf5e85f
Jonathan G. Westerfield <jgwesterfield@gmail.com>
2018-02-15 19:46:38 -0600

    Reformatted the DBInterface.php file to follow correct coding standards. I just made it easier to read the description of the functions

--- DBInterface.php
+++ DBInterface.php
@@ -10,20 +10,43 @@ include_once "DBAPI.php";
 
 interface DBInterface
 {
+    // class constructor
     public function __construct($db);
+
+    // class Destructor
     public function __destruct();
+    // prints out the entire database on the page in a table
     public function printEntireDB();
+
+    // returns the total number of walkers in the database
     public function getTotalNumWalkers();
+
+    // gets the number of walkers that walked through from the start to the end of the day
     public function getNumWalkersToday();
+
+    // gets the number of walkers from the start to the end of the week
     public function getNumWalkersThisWeek();
+
     //public function getHotTimes(); // gets the hours of day with peak traffic
     //public function getHotDays(); // gets the days with peak traffic for a week
-    public function getCurrentYearTraffic(); // Returns an array containing total number of walkers for each month for the current year
-    public function getTrafficByYear($year); // Returns an array containing total number of walkers for each month.
-    public function getCurrentMonthTraffic(); // Returns an array containing total number of walkers for each day for the current month
-    public function getTrafficByMonth($year, $month); // Returns an array containing total number of walkers for each day.
-    public function getCurrentDayTraffic(); // Returns an array containing total number of walkers for each hour for the current day
-    public function getTrafficByDay($year, $month, $day); // Returns an array containing total number of walkers for each hour
+
+    // Returns an array containing total number of walkers for each month for the current year
+    public function getCurrentYearTraffic();
+
+    // Returns an array containing total number of walkers for each month.
+    public function getTrafficByYear($year);
+
+    // Returns an array containing total number of walkers for each day for the current month
+    public function getCurrentMonthTraffic();
+
+    // Returns an array containing total number of walkers for each day.
+    public function getTrafficByMonth($year, $month);
+
+    // Returns an array containing total number of walkers for each hour for the current day
+    public function getCurrentDayTraffic();
+
+    // Returns an array (24) containing total number of walkers for each hour
+    public function getTrafficByDay($year, $month, $day);
 }
 
 ?>
