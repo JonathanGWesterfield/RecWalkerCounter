b9928489768c40d385aef19b30f342519888dc69
aoriver716 <jonathanbeechner@gmail.com>
2018-03-25 21:41:04 -0500

    Update stats.php

--- stats.php
+++ stats.php
@@ -5,6 +5,16 @@ include_once "DBInterface.php";
 $thisCommon = new Common(true);
 $db = new DBAPI($thisCommon);
 
+/**
+ * DBAPI constructor.
+ * @param $date_format
+ * @param $sub_division
+ * @param $data
+ *
+ * Displays statistics information of $data. $date_format is used for the header, and $sub_division
+ * is a textual representation of the time-length of one data point (eg. If you're looking at a day,
+ * then $sub_division = "hour").
+ */
 function display_stats($date_format, $sub_division, $data) {
     echo "<h3>Statistics for <u>" . $date_format . "</u></h3>";
     echo "<div class=\"stat\"><div class=\"stat-contents\"><div>Total Walkers</div><div>" . array_sum($data) . "</div></div></div>";
@@ -13,16 +23,19 @@ function display_stats($date_format, $sub_division, $data) {
     echo "<div class=\"stat\"><div class=\"stat-contents\"><div>Min Number of Walkers in an " . $sub_division . "</div><div>" . min($data) . "</div></div></div>";
 }
 
+// Check if the mode is set.
 if(!isset($_GET['mode'])) {
 
     echo '<script>console.log("Please specify a mode.");</script>';
 
+// Check if mode is a valid option.
 } elseif(strtolower($_GET['mode']) == 'day' || strtolower($_GET['mode']) == 'month'|| strtolower($_GET['mode']) == 'year') {
 
+    // Ensure that day, month, and year are all set.
     if(!isset($_GET['day']) || !isset($_GET['month']) || !isset($_GET['year'])) {
 
         echo '<script>console.log("Please specify a day, month, and year.");</script>';
-
+    // Display statistics.
     } else {
 
         $dateObj = DateTime::createFromFormat('d-m-Y|', $_GET['day'] . '-' . $_GET['month'] . '-' . $_GET['year']);
